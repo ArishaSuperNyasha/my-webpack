212609b2ca01018164836ca43fb963a1abc74a5c
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2023-11-30 19:34:06 +0300

    now you can add imgs in js-file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import example from './tY38OSyD0O8.jpg';
+
 console.log('Как интересно!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development', //Изменить на 'production' после завершения разработки
@@ -9,10 +10,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new HtmlWebpackPlugin({ //Создание из html файла html с подключёнными скриптами и стилями
-        title: 'webpack Boilerplate',
-        template: path.resolve(__dirname, './src/index.html'), 
-        filename: 'index.html',
+    new HtmlWebpackPlugin({ // Создание из html файла html с подключёнными скриптами и стилями
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/index.html'), 
+      filename: 'index.html',
     }),
-],
+    new CleanWebpackPlugin(), // Удаление старых файлов из dist при каждой сборке
+  ],
+  module: {
+    rules: [
+      { // изображения
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 }
